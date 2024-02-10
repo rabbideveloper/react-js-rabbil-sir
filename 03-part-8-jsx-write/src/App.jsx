3aa07402ed5d037bd23cd0cd4af882341a29720e
Rabbi-Sheikh <96rabby@gmail.com>
2024-02-10 16:05:48 +0600

    part 8 done- jsx syntext porichiti and practice done

---
+++ 03-part-8-jsx-write/src/App.jsx
@@ -0,0 +1,36 @@
+import "./App.css";
+
+function App() {
+	// const programmerName = "Rabbi Sheikh";
+
+	// const simpleAlert = () => {
+	// 	alert("hey rabbi are you ready ?");
+	// };
+
+	// function handleAlert() {
+	// 	alert("hey react developer");
+	// }
+
+	return (
+		<>
+			{/* <h1>Hello</h1>
+			<h1>{2 + 2}</h1>
+			<h1>Hello {programmerName} </h1>
+			<p>Right now time is {new Date().toLocaleTimeString()} </p> */}
+
+			{/* <button onClick={handleAlert}>Simple Alert</button> */}
+
+			<h1
+				style={{
+					color: "white",
+					textAlign: "center",
+					backgroundColor: "brown",
+					fontSize: "3rem",
+				}}>
+				hello rabbi
+			</h1>
+		</>
+	);
+}
+
+export default App;
